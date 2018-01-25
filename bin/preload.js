b1257464dfb6e4afd0ea4999171e08f7f0041e42
Ryan Murphy <rdmurphy@gmail.com>
2018-01-25 10:27:06 -0500

    Add compatVersion to the cli

--- bin/preload.js
+++ bin/preload.js
@@ -2,12 +2,14 @@
 
 const preload = require('../lib/preload')
 const CSON = require('season')
+const fs = require('fs')
 
 const arg = (alias, describe) => ({
   alias,
   describe
 })
 
+// barg = boolean arg
 const barg = (alias, describe, def) => ({
   alias,
   describe,
@@ -17,17 +19,27 @@ const barg = (alias, describe, def) => ({
 
 let args = require('yargs').options({
   r: arg('resources', 'path to the component or lib'),
-  d: arg('dest', 'destimation folder, before lib path'),
+  d: arg('dest', 'destination folder, before lib path'),
   m: barg('minify', 'minify the js', false),
   v: barg('verbose', 'verbose logging', false),
   c: arg('components', 'components glob'),
   l: arg('libraries', 'libraries glob'),
-  x: arg('config', 'config file')
+  x: arg('config', 'config file'),
+  j: barg('js', 'js mode', false)
 })
   .example('$0 -r app -d preload/ -c "**" -v')
   .help('h')
   .alias('m', 'compress')
   .argv
+
+if (args.c && fs.existsSync(args.c)) {
+  console.log(`Warning: Config file was given as option 'c' but it should be 'x'.`)
+  args.config = args.x = args.c
+  delete args.c
+  delete args.components
+}
+
+const originalArgs = args
 
 if (args.config === true) {
   args.config = 'preload.cson'
@@ -43,5 +55,8 @@ if (!(args.components || args.libraries)) {
   throw new Error('Please provide "components" and/or "libraries" argument.')
 }
 
-args.compress = args.minify
-preload(args)
+preload({
+  ...args,
+  compress: args.minify,
+  compatVersion: originalArgs.js ? 'edge' : 'json'
+})
